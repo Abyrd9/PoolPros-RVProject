15ec3a09a54520421892eff902fd7f9c14fc69e2
Andrew Byrd <andrew.byrd99@gmail.com>
2017-09-23 17:21:49 -0400

    refactoring to template string

--- js/index.js
+++ js/index.js
@@ -61,6 +61,7 @@ var filter = {
         console.log("filtered out")
       }
     })
+    cardModal.modalEventListener();
   },
 
   // Render card element with correct data from Json
@@ -70,30 +71,32 @@ var filter = {
     var cardContainer = document.querySelector(".card-container");
     cardContainer.append(card);
     // full HTML card element
-    var cardElement =  '  <div class="card-title-container">' +
-                       '    <h2 class="card-title">' + cardData.data.name + '</h2>' +
-                       '  </div>' +
-                       '  <div class="card-content-container">' +
-                       '    <div class="card-content-top">' +
-                       '      <div class="number-container">' +
-                       '        <i class="fa fa-phone" aria-hidden="true"></i>' +
-                       '        <p class="phone-number">' + cardData.data.phone1 + '</p>' +
-                       '      </div>' +
-                       '      <p class="number-sub-text">Can’t talk now? Click below to send an email.</p>' +
-                       '    </div>' +
-                       '    <div class="card-content-middle">' +
-                       '      <button class="card-button">' +
-                       '        <i class="fa fa-envelope" aria-hidden="true"></i>' +
-                       '        <p class="card-button-text">Contact this Pro</p>' +
-                       '      </button>' +
-                       '    </div>' +
-                       '    <div class="card-content-bottom">' +
-                       '      <h4 class="card-content-bottom-title">Business House</h4>' +
-                       '      <p class="card-content-bottom-text">Weekdays ' + cardData.data.weekHours.mon + '<br>Saturdays ' + dataSifting.saturdayCalc(cardData) + '<br>Sundays ' + dataSifting.sundayCalc(cardData) + '</p>' +
-                       '    </div>' +
-                       '  <div class="card-footer-container">' +
-                       '    <div class="card-footer-content-container">' + dataSifting.footerContentFilter(cardData) + '</div>' +
-                       '  </div>'
+    var cardElement = `
+    <div class="card-title-container">
+      <h2 class="card-title">${cardData.data.name}</h2>
+    </div>
+    <div class="card-content-container">
+      <div class="card-content-top">
+        <div class="number-container">
+          <i class="fa fa-phone" aria-hidden="true"></i>
+          <p class="phone-number">${cardData.data.phone1}</p>
+        </div>
+        <p class="number-sub-text">Can’t talk now? Click below to send an email.</p>
+      </div>
+      <div class="card-content-middle">
+        <button class="card-button">
+          <i class="fa fa-envelope" aria-hidden="true"></i>
+          <p class="card-button-text">Contact this Pro</p>
+        </button>
+      </div>
+      <div class="card-content-bottom">
+        <h4 class="card-content-bottom-title">Business House</h4>
+        <p class="card-content-bottom-text">Weekdays ${cardData.data.weekHours.mon}<br>Saturdays ${dataSifting.saturdayCalc(cardData)}<br>Sundays ${dataSifting.sundayCalc(cardData)}</p>
+      </div>
+    <div class="card-footer-container">
+      <div class="card-footer-content-container">${cardData.data.certifications.map(cert => dataSifting.footerContentFilter(cert)).join('')}</div>
+    </div>
+    `;
     //insert HTML card template into the card
     card.innerHTML = cardElement;
     console.log(card);
@@ -104,9 +107,9 @@ var dataSifting = {
   sundayCalc: function(cardData) {
     var weekHoursObj = cardData.data.weekHours;
     if (weekHoursObj.sun === "") {
-      return "- CLOSED"
+      return "- CLOSED";
     } else if (weekHoursObj.sun === "On Call") {
-      return "- " + weekHoursObj.sun
+      return `- ${weekHoursObj.sun}`;
     } else {
       return weekHoursObj.sun
     }
@@ -119,32 +122,115 @@ var dataSifting = {
       return weekHoursObj.sat
     }
   },
-  footerContentFilter: function(cardData) {
-    var cert = cardData.data.certifications;
-    var footerElements = {
-      Element: ""
+  footerContentFilter: function(cert) {
+    console.log(cert)
+    const starIcon = `<i class="fa fa-star fa-card-icon" aria-hidden="true"></i>`
+    const cogIcon = `<i class="fa fa-cog fa-card-icon" aria-hidden="true"></i>`
+    const houseIcon = `<i class="fa fa-home fa-card-icon" aria-hidden="true"></i>`
+    const userIcon = `<i class="fa fa-user fa-card-icon" aria-hidden="true"></i>`
+    if (cert === "Installation Pro") {
+      var currentIcon = starIcon;
+    } else if (cert === "Service Pro") {
+      var currentIcon = cogIcon;
+    } else if (cert === "Residential Pro") {
+      var currentIcon = houseIcon;
+    } else if (cert === "Commercial Pro") {
+      var currentIcon = userIcon;
     }
-    cert.forEach(function(e, i, arr) {
-      var starIcon = '<i class="fa fa-star fa-card-icon" aria-hidden="true"></i>'
-      var cogIcon = '<i class="fa fa-cog fa-card-icon" aria-hidden="true"></i>'
-      var houseIcon = '<i class="fa fa-home fa-card-icon" aria-hidden="true"></i>'
-      var userIcon = '<i class="fa fa-user fa-card-icon" aria-hidden="true"></i>'
-      var currentIcon = ""
-      if (e === "Installation Pro") {
-        currentIcon = starIcon;
-      } else if (e === "Service Pro") {
-        currentIcon = cogIcon;
-      } else if (e === "Residential Pro") {
-        currentIcon = houseIcon;
-      } else if (e === "Commercial Pro") {
-        currentIcon = userIcon;
-      }
-      footerElements.Element = footerElements.Element + '<div class="footer-text-container">' + currentIcon + '<p class="footer-text">' + cert[i] + '</p>' + '</div>'
-    })
-    console.log(footerElements.Element)
-    return footerElements.Element
+    return `<div class="footer-text-container">${currentIcon}<p class="footer-text">${cert}</p></div>`
   }
 
+}
+
+const cardModal = {
+  modalEventListener: function() {
+    const cardButtons = document.querySelectorAll(".card-button");
+    for (let i = 0; i < cardButtons.length; i++) {
+      cardButtons[i].addEventListener('click', e => {
+        if (e.target.classList.contains("card-button")) {
+          let button = e.target;
+          cardModal.createModal(button)
+        } else {
+          let button = e.target.parentNode;
+          cardModal.createModal(button)
+        }
+      })
+    }
+  },
+  createModal: function(button) {
+    const dealerTitle = button.parentNode.parentNode.parentNode.childNodes[1].childNodes[1].innerHTML;
+    const modal = `
+    <div class="full-modal-container">
+      <div class="modal-card">
+        <div class="modal-header-container">
+          <div class="modal-header-content-container">
+            <div class="modal-header-left-container">
+              <h4 class="modal-title-one">email</h4>
+              <h2 class="modal-title-two">dealer name</h2>
+            </div>
+            <div class="modal-header-right-container">
+              <i class="fa fa-times fa-modal-x" aria-hidden="true"></i>
+            </div>
+          </div>
+        </div>
+        <div class="modal-content-container">
+          <p class="modal-content-text">Fill out the form below and Premium Pools & Spas of Charlotte will get in touch.</p>
+          <form action="" class="modal-form-container">
+            <div class="modal-input-container">
+              <div class="input-title-container">
+                <h3 class="input-title">${dealerTitle}</h3>
+                <i class="fa fa-check fa-modal-check" aria-hidden="true"></i>
+              </div>
+              <input type="text" placeholder="" name="name" class="modal-input">
+            </div>
+            <div class="modal-input-container phone-number">
+              <div class="input-title-container">
+                <h3 class="input-title">phone number</h3>
+                <i class="fa fa-check fa-modal-check" aria-hidden="true"></i>
+              </div>
+              <input type="tel" placeholder="" name="phone-number" class="modal-input">
+            </div>
+            <div class="modal-input-container">
+              <div class="input-title-container">
+                <h3 class="input-title">email address</h3>
+                <i class="fa fa-check fa-modal-check" aria-hidden="true"></i>
+              </div>
+              <input type="text" placeholder="" name="email" class="modal-input">
+            </div>
+            <div class="modal-input-container">
+              <div class="input-title-container">
+                <h3 class="input-title">comments or questions</h3>
+                <p class="input-optional">optional</p>
+              </div>
+              <textarea rows="5" type="text" placeholder="" name="comments-questions" class="modal-input modal-textarea"></textarea>
+            </div>
+            <div class="modal-input-container">
+              <div class="input-title-container">
+                <h3 class="input-title">do you currently own a pool or spa?</h3>
+                <p class="input-optional">optional</p>
+              </div>
+              <div class="modal-button-container">
+                <input type="button" value="Yes" class="modal-input-button">
+                <input type="button" value="No" class="modal-input-button">
+              </div>
+            </div>
+            <div class="form-footer-container">
+              <button class="form-footer-button">
+                <p class="form-button-text">Send my email</p>
+                <i class="fa fa-chevron-right form-button-arrow" aria-hidden="true"></i>
+              </button>
+            </div>
+          </form>
+        </div>
+        <div class="modal-footer-container">
+          <p class="modal-footer-text">
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex.
+          </p>
+        </div>
+      </div>
+    </div>
+    `;
+  }
 }
 
 
